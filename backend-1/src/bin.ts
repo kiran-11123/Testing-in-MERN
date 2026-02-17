9906c680a02612adceb24a65d95b0a9568de2ef0
Kiran <kiran.sadara6730@gmail.com>
2026-02-17 20:45:44 +0530

    written the tests for api

---
+++ backend-1/src/bin.ts
@@ -0,0 +1,6 @@
+import {app} from './index';
+
+
+app.listen(5000 , ()=>{
+    console.log("Server is running in 5000 port")
+})
---
+++ backend-1/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+
+export const app = express()
+app.use(express.json())
+
+app.post("/sum" , (req,res)=>{
+     
+    const a  = req.body.a;
+    const b = req.body.b;
+
+    const answer = a+b;
+
+    return res.status(200).json({
+        answer
+    })
+})
+
+
+
+ 
+
+
+
