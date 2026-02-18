87a3c17c145a06969bd5453cf0333a31d66a4931
Kiran <kiran.sadara6730@gmail.com>
2026-02-18 18:51:09 +0530

    mocked the DB

--- backend-2/src/index.ts
+++ backend-2/src/index.ts
@@ -2,6 +2,10 @@ import express from 'express'
 import zod from 'zod'
 export const app = express()
 
+
+import { prisma } from './db'
+
+
 app.use(express.json())
 
 const sumInput = zod.object({
@@ -10,7 +14,7 @@ const sumInput = zod.object({
 })
 
 
-app.post("/sum" , (req,res)=>{
+app.post("/sum" , async (req,res)=>{
     const {a , b} = req.body;
 
     const parseResult = sumInput.safeParse({a , b});
@@ -19,5 +23,15 @@ app.post("/sum" , (req,res)=>{
         return res.status(400).json({error: "invalid input"})
     }
     const answer = a + b;
+
+   await  prisma.sum.create({
+        data:{
+            a,
+            b,
+            result: answer
+        }
+    })  
+
+
     return res.status(200).json({answer}    )
 })
--- backend-2/src/tests/index.test.ts
+++ backend-2/src/tests/index.test.ts
@@ -1,7 +1,17 @@
-import {describe , expect , it} from 'vitest'
+import {describe , expect , it ,vi} from 'vitest'
 import request from 'supertest'
 import { app } from '../index'
 
+// mocking the prisma client
+vi.mock('../db' , ()=>({
+  
+        prisma:{  
+            sum:{
+                create: vi.fn()
+            }
+        }
+    
+}))
 
 describe('POST /sum' , ()=>{
 
